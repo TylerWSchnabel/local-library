fa3336fe69fe5f98f4e325f582af4718d14bf52c
Tyler Schnabel <tylerwschnabel@gmail.com>
2023-03-24 11:23:58 -0700

    Basic setup of database

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -9,6 +9,15 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+const mongoDB = "mongodb+srv://tylerwschnabel:harry13@cluster0.msrwu4z.mongodb.net/local_library?retryWrites=true&w=majority";
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -21,6 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
